8d13691c7ee71ab1731a74fa7bda60ae384b7d5c
Tarek Bouzo <tarekbozo@gmail.com>
2021-03-16 18:37:13 +0100

    fixing ImageGallery

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ export * from './SEO';
 export * from './Layout';
 export * from './ImageGallery';
 export * from './ProductQuantityAdder';
-
 export * from './Input';
 export * from './Header';
 export * from './Cart';
